ffffeebd44f6ac4ba1156cc8792e718b0c8f4f49
João Pedro Medici <JooPedroDalla>
2026-09-15 09:30:39 -0300

    quase fim do handout

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # tecweb-2026-2-projeto1B
+# comandos úteis: python manage.py migrate ; python manage.py makemigrations ; env\Scripts\Activate.ps1 ; deactivate    
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Note
 
 
 def index(request):
-    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
+    if request.method == 'POST':
+        title = request.POST.get('titulo')
+        content = request.POST.get('detalhes')
+        # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
+        postit=Note(title=title,content=content).save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all()
+        return render(request, 'notes/index.html', {'notes': all_notes})
