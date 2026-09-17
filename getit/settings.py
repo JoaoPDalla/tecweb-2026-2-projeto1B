338ab69e8d31f3e8e5d0ba45553e4c52b380d640
João Pedro Medici <JooPedroDalla>
2026-09-17 09:11:07 -0300

    outra tentativa de deploy

--- getit/settings.py
+++ getit/settings.py
@@ -88,6 +88,16 @@ DATABASES = {
         ssl_require=not DEBUG
     )
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'getit',
+#         'USER': 'getituser',
+#         'PASSWORD': 'getitsenha',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
