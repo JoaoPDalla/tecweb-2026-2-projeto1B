a425214131d09b08c93c62201acefe74b4399458
João Pedro Medici <JooPedroDalla>
2026-09-19 23:36:07 -0300

    tentando

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path("notes/delete/<int:id>/", views.delete,name = 'delete'),
+    path("notes/update/<int:id>/",views.update, name = 'update'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -9,6 +9,23 @@ def index(request):
         # TAREFA: Utilize o title e content para criar um novo Note no banco de dados
         postit=Note(title=title,content=content).save()
         return redirect('index')
+    
     else:
         all_notes = Note.objects.all()
         return render(request, 'notes/index.html', {'notes': all_notes})
+
+def delete(request,id):
+    Nota = Note.objects.get(pk=id)
+
+    Nota.delete()
+
+    return redirect('index')
+
+def update(request, id):
+    note = Note.objects.get(pk=id)
+    if request.method == 'POST':
+        note.title = request.POST.get('titulo')
+        note.content = request.POST.get('detalhes')
+        note.save() 
+        return redirect('index')
+    return render(request, 'notes/update.html', {'note': note})
